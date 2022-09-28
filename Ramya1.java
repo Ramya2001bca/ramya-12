27662784ca6500ab9c1e75fd1edc43660f305f08
Ramya2001bca <113408822+Ramya2001bca@users.noreply.github.com>
2022-09-28 11:39:05 +0530

    Add files via upload

---
+++ Ramya1.java
@@ -0,0 +1,16 @@
+public class Ramya1{
+                public static void main(String[] args){
+                             ITEmployee vasanthi=new ITEmployee();
+                                  vasanthi.name="vasanthi";
+                                vasanthi.age=21;
+                          System.out.println(vasanthi.name);
+                                    System.out.println(vasanthi.age);
+                                vasanthi.morningwork(5);
+                        }
+}
+
+
+ 
+
+
+
